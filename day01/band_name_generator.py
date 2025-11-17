b196dce774c4b7ad112e8ac8373c5ea09b3bfe9c
Thomas Marshall <thomas1012m@gmail.com>
2025-11-16 21:50:48 -0600

    Add band name generator functionality

--- day01/band_name_generator.py
+++ day01/band_name_generator.py
@@ -1,1 +1,8 @@
+Greeting = "Hello this is the Band Name Generator!"
+print(Greeting)
+City = input("Please enter your City: \n")
+Pet = input("PLease enter the name of your pet: \n")
+
+
+print("Your Band Name could be", City + " " + Pet)
 
